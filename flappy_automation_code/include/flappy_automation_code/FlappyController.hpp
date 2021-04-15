61d3702028bd59669d5447272e907f57ecf94fc3
Oleg Gomozov <oleg.gomozov@easymile.com>
2021-04-15 22:02:22 +0200

    First successful flight
    - Added floor and ceiling detection
    - Added gate detection and visualisation
    - Added anticollision speed limiters
    - Added simple controller

--- flappy_automation_code/include/flappy_automation_code/FlappyController.hpp
+++ flappy_automation_code/include/flappy_automation_code/FlappyController.hpp
@@ -5,6 +5,19 @@
 #include <sensor_msgs/PointCloud.h>
 #include <deque>
 
+enum struct State{
+    IDLE,
+    FIND_FLOOR,
+    FIND_CEILING,
+    NAVIGATE,
+};
+
+
+struct SpeedLimit{
+    double vx;
+    double vy_up;
+    double vy_down;
+};
 
 class FlappyController {
 public:
@@ -18,13 +31,44 @@ private:
     ros::Subscriber m_sub_laser_scan;
     ros::Publisher m_pub_acc_cmd;
     ros::Publisher m_pub_point_cloud;
+    ros::Publisher m_pub_gate_upper;
+    ros::Publisher m_pub_gate_lower;
     ros::NodeHandlePtr m_handle_ptr;
 
+    bool isSimilarPointInCloud(const geometry_msgs::Point32& p);
     bool isPointGroundOrCeiling(const geometry_msgs::Point32& p);
-
+    ros::Time m_prev_laser_update;
     std::deque<geometry_msgs::Point32> m_points;
+
     geometry_msgs::Vector3 m_prev_speed;
-    ros::Time m_prev_time;
-    double m_vertical_offset_est;
+    ros::Time m_prev_speed_update_time;
+    double m_vertical_offset_est{0.0};
+
+    State m_state{State::IDLE};
+
+    double m_floor_offset;
+    double m_ceiling_offset;
+
+    ros::Timer m_timer;
+    void checkTimeouts(const ros::TimerEvent& e);
+
+
+    SpeedLimit getSpeedLimit();
+    bool detectPipe();
+
+
+    double m_pipe_start{0.0};
+    double m_pipe_end{0.0};
+    double m_pipe_gap_start{0.0};
+    double m_pipe_gap_end{0.0};
+    bool m_pipe_detected{false};
+
+    static constexpr auto m_dist_threshold = 0.05;
+    static constexpr auto m_global_speed_limit = 0.8;
+    static constexpr auto m_slow_speed = 0.1;
+    static constexpr auto m_margin = 0.25;
+    static constexpr auto m_nom_acc = 0.1;
+    static constexpr auto m_max_acc = 0.3;
+    static constexpr auto m_pipe_width = 0.45;
 
 };
--- flappy_automation_code/src/FlappyController.cpp
+++ flappy_automation_code/src/FlappyController.cpp
@@ -1,5 +1,6 @@
 #include <flappy_automation_code/FlappyController.hpp>
 #include <laser_geometry/laser_geometry.h>
+#include <geometry_msgs/PolygonStamped.h>
 
 
 FlappyController::FlappyController(ros::NodeHandlePtr h) :
@@ -11,23 +12,36 @@ FlappyController::FlappyController(ros::NodeHandlePtr h) :
       h->advertise<geometry_msgs::Vector3>("/flappy_acc", 1)},
     m_pub_point_cloud{
       h->advertise<sensor_msgs::PointCloud>("/flappy_point_cloud", 1)},
-    m_prev_time{ros::Time::now()},
-    m_vertical_offset_est{0.0},
+    m_pub_gate_upper{h->advertise<geometry_msgs::PolygonStamped>("/flappy_gate_upper", 1)},
+    m_pub_gate_lower{h->advertise<geometry_msgs::PolygonStamped>("/flappy_gate_lower", 1)},
     m_handle_ptr{std::move(h)}
-{}
+{
+    m_timer = m_handle_ptr->createTimer(ros::Duration(0.1), &FlappyController::checkTimeouts, this, false, false);
+}
 
 
 void FlappyController::velCallback(const geometry_msgs::Vector3::ConstPtr& msg) {
 
+    if (m_state == State::IDLE){
+        m_state = State::FIND_FLOOR;
+        m_prev_speed_update_time = ros::Time::now();
+        m_prev_speed = *msg;
+        m_vertical_offset_est = 0.0;
+        m_points.clear();
+        m_timer.start();
+        ROS_INFO("Starting up. First lets find the floor.");
+        return;
+    }
+
     // Estimate how have we moved wrt the last frame
     const auto new_time = ros::Time::now();
     const auto& new_speed = *msg;
-    const auto delta = (new_time - m_prev_time).toSec();
+    const auto delta = (new_time - m_prev_speed_update_time).toSec();
 
     const auto dx = (m_prev_speed.x + new_speed.x) * 0.5 * delta;
     const auto dy = (m_prev_speed.y + new_speed.y) * 0.5 * delta;
 
-    m_prev_time = new_time;
+    m_prev_speed_update_time = new_time;
     m_prev_speed = new_speed;
 
     // Shift the points in the opposite direction
@@ -37,15 +51,106 @@ void FlappyController::velCallback(const geometry_msgs::Vector3::ConstPtr& msg) 
         point.y -= dy;
     }
 
-    // Destroy points that we have passed. Since we are moving only from left to right
-    // the points with smaller x coordinate are always in the beginning of the deque.
-    while((not m_points.empty() and m_points.front().x < -0.2))
+    // Destroy points that we have passed. The points are sorted by x in the laser callback.
+    while((not m_points.empty() and m_points.front().x < -m_margin))
         m_points.pop_front();
 
+    // Draw outline
+    if (m_state == State::NAVIGATE){
+        geometry_msgs::PolygonStamped upper_poly_msg;
+        upper_poly_msg.header.frame_id = "world";
+        if (m_pipe_detected){
+            auto& points = upper_poly_msg.polygon.points;
+            points.resize(4);
+            points[0].x = m_pipe_end;
+            points[0].y = m_ceiling_offset - m_vertical_offset_est;
+            points[1].x = m_pipe_start;
+            points[1].y = m_ceiling_offset - m_vertical_offset_est;
+            points[2].x = m_pipe_start;
+            points[2].y = m_pipe_gap_end;
+            points[3].x = m_pipe_end;
+            points[3].y = m_pipe_gap_end;
+        }
+        m_pub_gate_upper.publish(upper_poly_msg);
+        geometry_msgs::PolygonStamped lower_poly_msg;
+        lower_poly_msg.header.frame_id = "world";
+        if (m_pipe_detected){
+            auto& points = lower_poly_msg.polygon.points;
+            points.resize(4);
+            points[0].x = m_pipe_end;
+            points[0].y = m_pipe_gap_start;
+            points[1].x = m_pipe_start;
+            points[1].y = m_pipe_gap_start;
+            points[2].x = m_pipe_start;
+            points[2].y = m_floor_offset - m_vertical_offset_est;
+            points[3].x = m_pipe_end;
+            points[3].y = m_floor_offset - m_vertical_offset_est;
+        }
+        m_pub_gate_lower.publish(lower_poly_msg);
+
+
+    }
+
+
+    // Check if we found the floor
+    if (m_state == State::FIND_FLOOR){
+        const auto it = std::min_element(m_points.begin(), m_points.end(), [](const auto& a, const auto& b){
+            return a.y < b.y;
+        });
+        if (it != m_points.end() and it->y < 0){
+            m_floor_offset = m_vertical_offset_est + it->y;
+            m_state = State::FIND_CEILING;
+            ROS_INFO("Found floor at %f. Switching to finding ceiling.", m_floor_offset);
+        }
+    }
+
+    if (m_state == State::FIND_CEILING){
+        const auto it = std::max_element(m_points.begin(), m_points.end(), [](const auto& a, const auto& b){
+            return a.y < b.y;
+        });
+
+        if (it != m_points.end() and it->y > 0){
+            m_ceiling_offset = m_vertical_offset_est + it->y;
+            m_state = State::NAVIGATE;
+            m_points.clear();
+            ROS_INFO("Found ceiling at %f. Starting navigation", m_ceiling_offset);
+        }
+    }
+
+
     // Publish command
+    auto vx_des = 0.0;
+    auto vy_des = 0.0;
+    if (m_state == State::FIND_FLOOR) {
+        vx_des = 0.0;
+        vy_des = -m_slow_speed;
+    }
+    if (m_state == State::FIND_CEILING){
+        vx_des =  0.0;
+        vy_des =  m_slow_speed;
+    }
+    if (m_state == State::NAVIGATE){
+        vx_des =  m_global_speed_limit;
+        if (not (m_pipe_detected) or m_pipe_end < -m_margin) {
+            vy_des = 0.0;
+        } else {
+            const auto y_target = (m_pipe_gap_start + m_pipe_gap_end) * 0.5;
+            vy_des = 0.8 * y_target / delta;
+        }
+
+    }
+
+    const auto speed_limit = getSpeedLimit();
+    const auto vx_prev = m_prev_speed.x;
+    const auto vy_prev = m_prev_speed.y;
+
+    const auto vx_next = std::clamp(vx_des, 0.0, speed_limit.vx);
+    const auto vy_next = std::clamp(vy_des, speed_limit.vy_down, speed_limit.vy_up);
+
     geometry_msgs::Vector3 acc_cmd;
-    acc_cmd.x = 0;
-    acc_cmd.y = 0;
+    assert(delta > 0);
+    acc_cmd.x = std::clamp((vx_next-vx_prev)/delta,-m_max_acc, m_max_acc);
+    acc_cmd.y = std::clamp((vy_next-vy_prev)/delta,-m_max_acc, m_max_acc);
     m_pub_acc_cmd.publish(acc_cmd);
 
     //TODO publish bird transform
@@ -53,16 +158,24 @@ void FlappyController::velCallback(const geometry_msgs::Vector3::ConstPtr& msg) 
 
 
 void FlappyController::laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    if (m_state == State::IDLE)
+        return;
+
+    m_prev_laser_update = ros::Time::now();
     // Convert rays to points
     auto point_cloud_msg = sensor_msgs::PointCloud{};
     laser_geometry::LaserProjection{}.projectLaser(*msg, point_cloud_msg);
 
     // Filter out points outside corridors bounds and add to the cloud
-    // TODO do not add points in another point is at the same location
     auto& points = point_cloud_msg.points;
     std::copy_if(points.begin(), points.end(), std::back_inserter(m_points), [this](const auto& point){
-        return not isPointGroundOrCeiling(point);
+        return not isPointGroundOrCeiling(point) and not isSimilarPointInCloud(point);
     });
+    std::sort(m_points.begin(), m_points.end(), [](const auto& a, const auto& b){
+        return a.x < b.x;
+    });
+
+    m_pipe_detected = detectPipe();
 
     // Publish current cloud for debug
     point_cloud_msg.points.clear();
@@ -73,5 +186,104 @@ void FlappyController::laserScanCallback(const sensor_msgs::LaserScan::ConstPtr&
 
 bool FlappyController::isPointGroundOrCeiling(const geometry_msgs::Point32& p) {
     // TODO make a proper ground detection
+    if (m_state == State::NAVIGATE){
+        if (p.y + m_vertical_offset_est < m_floor_offset + 0.1)
+            return true;
+        if (p.y + m_vertical_offset_est > m_ceiling_offset - 0.1)
+            return true;
+    }
     return false;
 };
+
+
+bool FlappyController::isSimilarPointInCloud(const geometry_msgs::Point32& p){
+    for (const auto& other : m_points){
+        const auto dx = p.x - other.x;
+        const auto dy = p.y - other.y;
+        const auto dist = dx*dx + dy*dy;
+        if (dist < m_dist_threshold*m_dist_threshold)
+            return true;
+    }
+    return false;
+}
+
+
+void FlappyController::checkTimeouts(const ros::TimerEvent& e){
+    const auto now = ros::Time::now();
+    if ((now - m_prev_speed_update_time).toSec() > 0.2){
+        m_state = State::IDLE;
+        m_timer.stop();
+        ROS_ERROR("Timeout on speed update. Switching to idle.");
+        return;
+    }
+    if ((now - m_prev_laser_update).toSec() > 0.2){
+        m_state = State::IDLE;
+        m_timer.stop();
+        ROS_ERROR("Timeout on laser scan update. Switching to idle.");
+        return;
+    }
+}
+
+
+SpeedLimit FlappyController::getSpeedLimit(){
+
+    auto res = SpeedLimit{
+        m_global_speed_limit,
+        m_global_speed_limit,
+        -m_global_speed_limit
+    };
+
+    if (m_points.empty())
+        return res;
+
+    const auto max_speed_from_dist = [&](auto dist){
+        const auto v_sq = std::max(0.0, 2.0 * m_nom_acc * dist);
+        return std::sqrt(v_sq);
+    };
+
+    for (const auto& p : m_points){
+        if (p.y < m_margin and p.y > -m_margin)
+            res.vx = std::min(max_speed_from_dist(p.x - m_margin), res.vx);
+        if (p.x < m_margin and p.y > 0)
+            res.vy_up = std::min(max_speed_from_dist(p.y - m_margin), res.vy_up);
+        if (p.x < m_margin and p.y <= 0)
+            res.vy_down = std::max(-max_speed_from_dist(-p.y - m_margin), res.vy_down);
+    }
+
+    return res;
+}
+
+
+
+bool FlappyController::detectPipe(){
+    if (m_state != State::NAVIGATE)
+        return false;
+
+    if (m_points.empty())
+        return false;
+
+    m_pipe_start = m_points.front().x;
+    const auto pipe_end = std::lower_bound(m_points.begin(), m_points.end(), m_pipe_start + m_pipe_width, [](const auto& p, auto v){
+        return p.x < v;
+    });
+    if (pipe_end == m_points.end())
+        return false;
+    m_pipe_end = std::prev(pipe_end)->x;
+
+    auto offsets = std::vector<double>{};
+    offsets.reserve(std::distance(m_points.begin(), pipe_end));
+    std::transform(m_points.begin(), pipe_end, std::back_inserter(offsets), [](const auto& p){
+        return p.y;
+    });
+    std::sort(offsets.begin(), offsets.end());
+    for (auto it = offsets.begin(); it != std::prev(offsets.end()); ++it){
+        const auto diff = *std::next(it) - *it;
+        if (diff > m_margin*2) {
+            m_pipe_gap_start = *it;
+            m_pipe_gap_end = *std::next(it);
+        }
+
+    }
+
+    return true;
+}
